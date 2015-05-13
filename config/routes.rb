33684d88c62d8f4bcddf598eac72627f23bf61b7
tylerheintz <tyler@heintznet.com>
2015-05-13 18:10:04 +0000

    kevin homepage

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,8 @@ Rails.application.routes.draw do
 
   resources :users
 
+  
+  root :to => 'comments#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
